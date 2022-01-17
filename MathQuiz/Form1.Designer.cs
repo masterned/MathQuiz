1edddac1686e9bd59c6c7f5a030f1c1b83fb7fbb
Spencer Dent <sd00070@my.westga.edu>
2022-01-16 23:59:04 -0500

    fix changed size

--- MathQuiz/Form1.Designer.cs
+++ MathQuiz/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace MathQuiz
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.ClientSize = new System.Drawing.Size(480, 357);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
             this.Name = "Form1";
